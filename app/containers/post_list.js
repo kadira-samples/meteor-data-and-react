40d76fe864cd5fabb18467f1fd56f13a757e48d1
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-02-19 09:57:42 +0530

    Add SEO tags

--- app/containers/post_list.js
+++ app/containers/post_list.js
@@ -1,5 +1,6 @@
 import {composeWithTracker} from 'react-komposer';
 import PostList from '../components/post_list.jsx';
+import { DocHead } from 'meteor/kadira:dochead';
 
 function composer(props, onData) {
   const handle = Meteor.subscribe('posts');
@@ -7,6 +8,8 @@ function composer(props, onData) {
     const posts = Posts.find({}, {sort: {_id: 1}}).fetch();
     onData(null, {posts});
   };
+
+  DocHead.setTitle('My Blog');
 };
 
 export default composeWithTracker(composer)(PostList);
--- app/containers/post_page.js
+++ app/containers/post_page.js
@@ -1,11 +1,19 @@
-import {composeWithTracker} from 'react-komposer';
+import { composeWithTracker } from 'react-komposer';
 import PostPage from '../components/post_page.jsx';
+import { DocHead } from 'meteor/kadira:dochead';
 
 function composer(props, onData) {
   const handle = Meteor.subscribe('singlePost', props._id);
+
   if(handle.ready()) {
     const post = Posts.findOne({_id: props._id});
     onData(null, {post});
+
+    // Make our blog posts SEO friendly
+    DocHead.setTitle(post.title);
+    DocHead.addMeta({
+      name: 'description', content: post.content.substr(0, 20)
+    });
   }
 };
 
