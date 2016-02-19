3d2d52da7ccee0de54d8f462ddee79e376108f52
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-02-19 12:09:07 +0530

    Implements a NoSSR block

---
+++ app/components/comments.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Waiting = () => (
+  <div>
+    Waiting for comments ...
+  </div>
+);
+
+const Comments = () => (<div>
+  No comments yet!
+</div>);
+
+export default Comments;
--- app/components/post_page.jsx
+++ app/components/post_page.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import NoSSR from 'react-no-ssr';
+import Comments from './comments.jsx';
+import { Waiting } from './comments.jsx';
 
 const PostPage = ({post}) => (
   <div>
     <a href={FlowRouter.path('/')}>Back</a>
     <h3>{post.title}</h3>
     <p>{post.content}</p>
+    <hr />
+
+    <NoSSR onSSR={<Waiting />}>
+      <Comments />
+    </NoSSR>
   </div>
 );
 
